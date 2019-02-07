5e8a5465a676e17ee0812bf6ff68c6d757b404c0
rleongPd5 <rleong00@stuy.edu>
2019-02-07 17:52:29 -0500

    updated test case

--- Driver.java
+++ Driver.java
@@ -1,6 +1,6 @@
 public class Driver{
   public static void main(String[] args){
-    QueenBoard q = new Queenboard(4); //Make an empty board size 4x4
+    QueenBoard q = new QueenBoard(4); //Make an empty board size 4x4
     //-1 means there is a queen
     /*Starting board
     [0][0][0][0]
@@ -8,7 +8,8 @@ public class Driver{
     [0][0][0][0]
     [0][0][0][0]
     */
-    QueenBoard.addQueen(3, 3)
+    q.addQueen(3, 3);
+    System.out.println(q.toString());
     /*Resulting board
     [0][0][0][0]
     [0][0][0][0]
