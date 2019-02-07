35276232851d38c3700d2435ea5b7211e5b6107a
rleongPd5 <rleong00@stuy.edu>
2019-02-07 09:55:31 -0500

    Added test case for addQueen and constructor

--- Driver.java
+++ Driver.java
@@ -1,5 +1,19 @@
 public class Driver{
   public static void main(String[] args){
-    
+    QueenBoard q = new Queenboard(4); //Make an empty board size 4x4
+    //-1 means there is a queen
+    /*Starting board
+    [0][0][0][0]
+    [0][0][0][0]
+    [0][0][0][0]
+    [0][0][0][0]
+    */
+    QueenBoard.addQueen(3, 3)
+    /*Resulting board
+    [0][0][0][0]
+    [0][0][0][0]
+    [0][0][0][0]
+    [0][0][0][-1]
+    */
   }
 }
