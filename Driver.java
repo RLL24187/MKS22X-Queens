5ff5e51f025deee03c0ff5cbca69365e763533a6
rleongPd5 <rleong00@stuy.edu>
2019-02-07 21:48:13 -0500

    Edited and added test print statements

--- Driver.java
+++ Driver.java
@@ -8,10 +8,14 @@ public class Driver{
     [0][0][0][0]
     [0][0][0][0]
     */
+    System.out.println("Starting board 4x4: \n"+q.toString());
+    System.out.println("Checking if all values are 0: true --> "+q.allZero());
+    System.out.println("Testing toStringDebug: \n"+q.toStringDebug());
+    System.out.println("Adding queen to (3,3): true --> "+q.addQueen(3, 3));
     System.out.println(q.toString());
-    q.addQueen(3, 3);
+    System.out.println("Attempting to add queen to (3,3) again: false --> "+q.addQueen(3, 3));
     System.out.println(q.toString());
-    System.out.println(q.toStringDebug());
+    System.out.println("Checking if all values are 0: false --> "+q.allZero());
     /*Resulting board
     [0][0][0][0]
     [0][0][0][0]
