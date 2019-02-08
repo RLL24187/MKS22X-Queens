5ae94718a6fe8765f6b0e24dd66dc2b8ba1a506a
rleongPd5 <rleong00@stuy.edu>
2019-02-07 21:47:58 -0500

    Added allZero

--- QueenBoard.java
+++ QueenBoard.java
@@ -33,7 +33,7 @@ public class QueenBoard{
   }
 
   public boolean removeQueen(int r, int c){
-    if (r < board.length && r>=0 && c>=0 && c < board.length && queenHere){
+    if (r < board.length && r>=0 && c>=0 && c < board.length && queenHere(r, c)){
       board[r][c] = 0;
       return true;
     }
@@ -94,12 +94,25 @@ public class QueenBoard{
     }
     return output;
   }
+
+  //Checks to see if all spaces are 0s
+  public boolean allZero(){
+    for (int i = 0; i < board.length; i++){
+      for (int j = 0; j < board.length; j++){
+        if (board[i][j]!=0){
+          return false;
+        }
+      }
+    }
+    return true;
+  }
   /**
   *@return false when the board is not solveable and leaves the board filled with zeros;
   *        true when the board is solveable, and leaves the board in a solved state
   *@throws IllegalStateException when the board starts with any non-zero value
   */
   public boolean solve(){
+
     return true;
   }
 
